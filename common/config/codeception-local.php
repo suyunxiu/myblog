4b744d47e073d5906f6f9a7b401df2080e924cbe
minchao <373094783@qq.com>
2019-06-17 16:22:22 +0800

    ignore

---
+++ common/config/codeception-local.php
@@ -0,0 +1,16 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/main.php',
+    require __DIR__ . '/main-local.php',
+    require __DIR__ . '/test.php',
+    require __DIR__ . '/test-local.php',
+    [
+        'components' => [
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => 'HI9L6dhc6n2rH6mkys5nlRPvMXO71dPo',
+            ],
+        ],
+    ]
+);
